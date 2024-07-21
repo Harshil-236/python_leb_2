d96827d8d6f9a15a7f1a57b6a6edad055357c736
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-07-21 23:15:27 +0530

    Update readwrite.py

--- readwrite.py
+++ readwrite.py
@@ -6,3 +6,6 @@ with open('example.txt', 'w') as file:
 with open('example.txt', 'r') as file:
     content = file.read()
     print(content)
+
+output:-
+![Screenshot (42)](https://github.com/user-attachments/assets/24e054e3-8a52-4945-a5c9-a1a876d31a50)
