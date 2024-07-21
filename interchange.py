dc7d3d1a10ca188813bf06ec0883f8ee4e6e79f5
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-07-21 21:50:46 +0530

    Create interchange.py

---
+++ interchange.py
@@ -0,0 +1,13 @@
+def interchange_first_last(input_list):
+    if len(input_list) >= 2:
+        input_list[0], input_list[-1] = input_list[-1], input_list[0]
+    return input_list
+
+input_list = [12, 35, 9, 56, 24]
+
+output_list = interchange_first_last(input_list)
+
+print(output_list)
+
+output:-
+
