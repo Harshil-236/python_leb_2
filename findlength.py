3c5582e7b1a7950b347a2ee834c32931a44e7042
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-07-21 22:01:57 +0530

    Update findlength.py

--- findlength.py
+++ findlength.py
@@ -5,3 +5,4 @@ list_length = len(my_list)
 print("The length of the list is:", list_length)
 
 output:-
+![Screenshot (39)](https://github.com/user-attachments/assets/8d404c85-d26b-476e-85c5-1183490dffd4)
