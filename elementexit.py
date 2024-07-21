31578537d58235fa83f2b85b070658400151e4fa
Harshil-236 <bca2022harshil1780@tnraocollege.org>
2024-07-21 22:05:29 +0530

    Create elementexit.py

---
+++ elementexit.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 5]
+
+element_to_find = 3
+
+if element_to_find in my_list:
+    print(f"{element_to_find} exists in the list.")
+else:
+    print(f"{element_to_find} does not exist in the list.")
+
+output:-
